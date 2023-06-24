dabe5a789889b1b9eba3e3739689a8251cca9292
uthen_sungun <uthengaming@gmail.com>
2023-06-24 13:59:07 +0700

    กำหนดสิทธิ์การเข้าถึงเมนูต่างๆและหน้า home

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JwtAuthService } from '../services/auth/jwt-auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  constructor(private jwtAuth: JwtAuthService) {
+    this.jwtAuth.checkTokenIsValid().subscribe((res) => {
+      this.userdata = res;
+    });
+  }
+  userdata?: any = {
+    email: null,
+    first_name: null,
+    last_name: null,
+    password: null,
+    role: null,
+    __v: null,
+    _id: null,
+  };
 }
--- src/app/services/layout/admin-layout/admin-layout.component.ts
+++ src/app/services/layout/admin-layout/admin-layout.component.ts
@@ -1,24 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JwtAuthService } from '../../auth/jwt-auth.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.css']
+  styleUrls: ['./admin-layout.component.css'],
 })
-export class AdminLayoutComponent {
-  constructor(
-    private jwtAuth: JwtAuthService,
-  ) {
-    this.jwtAuth.checkTokenIsValid().subscribe(res => {
-      this.userdata.username = res.first_name+' '+res.last_name
+export class AdminLayoutComponent implements OnInit {
+  constructor(private jwtAuth: JwtAuthService) {
+    this.jwtAuth.checkTokenIsValid().subscribe((res) => {
+      this.userdata = res;
+      this.expiredate = 99;
     });
   }
 
-  userdata: any = {
-    username: null
-  }
+  userdata?: any = {
+    email: null,
+    first_name: null,
+    last_name: null,
+    password: null,
+    role: null,
+    __v: null,
+    _id: null,
+  };
+
+  expiredate:number = 0;
+
+  ngOnInit(): void {}
   signout() {
     Swal.fire({
       title: 'ต้องการออกจากระบบใช่หรือไม่',
@@ -29,10 +38,9 @@ export class AdminLayoutComponent {
     }).then((result) => {
       /* Read more about isConfirmed, isDenied below */
       if (result.isConfirmed) {
-        this.jwtAuth.signout()
+        this.jwtAuth.signout();
       } else if (result.isDenied) {
-
       }
-    })
+    });
   }
 }
